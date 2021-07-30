c7adcdfeec7a423134f22f33956f7a5a9a5a2897
Geoffrey Biggs <gbiggs@killbots.net>
2021-07-30 13:48:27 +0900

    Add reading-related interfaces
    Signed-off-by: Geoffrey Biggs <gbiggs@killbots.net>

---
+++ rosbag2_cpp/include/rosbag2_cpp/reader_interfaces/filtered_reader_interface.hpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef ROSBAG2_CPP__READER_INTERFACES_FILTERED_READER_INTERFACE_HPP_
+#define ROSBAG2_CPP__READER_INTERFACES_FILTERED_READER_INTERFACE_HPP_
+
+#include <memory>
+#include <vector>
+
+#include "rosbag2_cpp/converter_options.hpp"
+#include "rosbag2_cpp/visibility_control.hpp"
+
+#include "rosbag2_storage/bag_metadata.hpp"
+#include "rosbag2_storage/serialized_bag_message.hpp"
+#include "rosbag2_storage/storage_filter.hpp"
+#include "rosbag2_storage/storage_options.hpp"
+#include "rosbag2_storage/topic_metadata.hpp"
+
+namespace rosbag2_cpp
+{
+namespace reader_interfaces
+{
+
+class ROSBAG2_CPP_PUBLIC FilteredReaderInterface : public BaseReaderInterface
+{
+public:
+  FilteredReaderInterface()
+    : BaseReaderInterface()
+  {}
+
+  virtual ~FilteredReaderInterface() {}
+
+  virtual void set_filter(const rosbag2_storage::StorageFilter & storage_filter) = 0;
+
+  virtual void reset_filter() = 0;
+};
+
+}  // namespace reader_interfaces
+}  // namespace rosbag2_cpp
+
+#endif  // ROSBAG2_CPP__READER_INTERFACES_FILTERED_READER_INTERFACE_HPP_
